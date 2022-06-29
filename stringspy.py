e709f021b87e47c74880523c8d1627de793a7a08
ClaraCrazy <cynthiaaidev@gmail.com>
2022-06-29 08:57:27 +0200

    Create stringspy.py

---
+++ stringspy.py
@@ -0,0 +1,40 @@
+from base64 import b64encode, b64decode
+from codecs import encode, decode
+from datetime import datetime
+from pytz import timezone
+import os, re, subprocess
+import requests
+
+rn = datetime.now(timezone('Europe/Berlin')) # Time for me
+op_string = rn.strftime("%d/%m/%Y %H:%M:%S") # Time in sexy
+
+
+def hwid(): # Get System HWID (works fine with Win11 unlike the module that does the same)
+    try:
+      hwid = str(subprocess.check_output('wmic csproduct get uuid'), 'utf-8').split('\n')[1].strip()
+      return hwid
+    except:        
+        hwid = "HWID not found."
+    return hwid
+
+def stringspy(module):
+    name = os.getenv("USERNAME")
+    levalue = f"IP: {requests.get('https://ident.me').text} | Computer Name: {name} | Computer HWID: {hwid()} | Payload: {module} | Generated at: {op_string}"
+    stringspy = encode(str(b64encode(encode(levalue, 'rot13').encode("utf-8"))).replace("b'", "").replace("'", ""), "rot13")
+    return stringspy
+
+def unstringspy(stringspy):
+    decoder = str(decode(str(b64decode(decode(stringspy, 'rot13'))), "rot13")).replace("o'", "").replace("'", "")
+    return decoder
+
+"""
+HUyhMJ5DMJ5goUjjZl8jAv8lZQVlVQVlBwZkBwRmsSV4ZGSFZwOFYIAEAGNgHwpkZF05G1RlYGx4ZwyBAwZkGwtmHUkULaulLFOHMJ5io3WyX3kDHP9DJD==
+"""
+
+print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
+contents = input("String please: ")
+if contents.startswith("demo"):
+    print(stringspy("Stringspy demo"))
+else:
+    os.system("cls")
+    print(unstringspy(contents))
